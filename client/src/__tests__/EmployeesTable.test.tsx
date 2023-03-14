053392759057c9652b164b810759f5e48e149bd1
jackb14 <jack@26385.xyz>
2023-03-14 11:11:37 +0000

    add github actions yml

--- client/src/__tests__/EmployeesTable.test.tsx
+++ client/src/__tests__/EmployeesTable.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { render } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import { act } from "react-dom/test-utils";
--- client/src/components/EmployeesTable.tsx
+++ client/src/components/EmployeesTable.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import helper from "../helper";
 import { AiOutlineUserDelete } from "react-icons/ai";
