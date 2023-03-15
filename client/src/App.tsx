9ac63b7bdff4030fba14c1f06c1aaa461be3b8ed
sohyun <shlim0410@gmail.com>
2023-03-15 16:09:33 +0000

    feat: add authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,30 @@
 import './App.css';
 import Nav from './components/Nav';
 import Redirect from './components/Redirect';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
 
-
+const clerk_pub_key = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY!;
 
 function App () {
   return (
-    <div className="App">
-      <Nav/>
-      <Redirect/>
-    </div>
+    <ClerkProvider publishableKey={clerk_pub_key}>
+      <div className="App">
+         <SignedIn>
+            <Nav/>
+            <Redirect />
+          </SignedIn>
+         <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
+      </div>
+    </ClerkProvider>
   );
 }
 
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -4,6 +4,7 @@ import { BsFillPeopleFill } from "react-icons/bs";
 import { AiFillClockCircle } from "react-icons/ai";
 import { ImCalendar } from "react-icons/im";
 import { IoIosCreate } from "react-icons/io";
+import SignOutButton from './Signout';
 
 let activeStyle = {
   backgroundColor: "rgb(35, 39, 42)",
@@ -11,59 +12,62 @@ let activeStyle = {
 
 function Nav() {
   return (
-    <nav className="Nav">
-      <ul className="nav-list">
-        <li className={`nav-row`}>
-          <NavLink
-            to="/"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            <AiFillHome />
-            <p> Home </p>
-          </NavLink>
-        </li>
+    <div className="Navbar" style={{ display: "flex", flexDirection: "column", justifyContent:"space-between"}}>
+        <nav className="Nav">
+        <ul className="nav-list">
+          <li className={`nav-row`}>
+            <NavLink
+              to="/"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              <AiFillHome />
+              <p> Home </p>
+            </NavLink>
+          </li>
 
-        <li className={`nav-row`}>
-          <NavLink
-            to="/employees"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            <BsFillPeopleFill />
-            <p>Employees</p>
-          </NavLink>
-        </li>
+          <li className={`nav-row`}>
+            <NavLink
+              to="/employees"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              <BsFillPeopleFill />
+              <p>Employees</p>
+            </NavLink>
+          </li>
 
-        <li className={`nav-row`}>
-          <NavLink
-            to="/shift-types"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            <AiFillClockCircle />
-            <p>Shift Types</p>
-          </NavLink>
-        </li>
+          <li className={`nav-row`}>
+            <NavLink
+              to="/shift-types"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              <AiFillClockCircle />
+              <p>Shift Types</p>
+            </NavLink>
+          </li>
 
-        <li className={`nav-row`}>
-          <NavLink
-            to="/shifts"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            <IoIosCreate />
-            <p>Shifts</p>
-          </NavLink>
-        </li>
+          <li className={`nav-row`}>
+            <NavLink
+              to="/shifts"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              <IoIosCreate />
+              <p>Shifts</p>
+            </NavLink>
+          </li>
 
-        <li className={`nav-row`}>
-          <NavLink
-            to="/rota"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            <ImCalendar />
-            <p>Rota</p>
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+          <li className={`nav-row`}>
+            <NavLink
+              to="/rota"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              <ImCalendar />
+              <p>Rota</p>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <SignOutButton />
+    </div>
   );
 }
 
